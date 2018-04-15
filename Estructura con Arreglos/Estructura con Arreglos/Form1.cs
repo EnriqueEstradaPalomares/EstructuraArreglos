b7c708d68bfc791e9aa01389b5493c8fc0bb27ec
EnriqueEstradaPalomares <36722043+EnriqueEstradaPalomares@users.noreply.github.com>
2018-04-15 14:15:25 -0500

    Add files via upload

---
+++ Estructura con Arreglos/Estructura con Arreglos/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estructura_con_Arreglos
+{
+    public partial class Form1 : Form
+    {
+        Productos prod = new Productos();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            prod.llenar(txtCodigo.Text, txtNombre.Text, txtDesc.Text, 
+                txtCantidad.Text, txtCosto.Text);
+            txtLista.Text = "Producto Agregado";
+            limpiarTxt();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            txtLista.Text = "Resultado de Busqueda \r\n" + prod.buscar(txtCodigo.Text);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            prod.insertar(txtCodigo.Text, txtNombre.Text, txtDesc.Text,
+                txtCantidad.Text, txtCosto.Text,Convert.ToInt16(txtPos.Text));
+            txtLista.Text = "Producto insertado";
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            txtLista.Text = prod.Listar();
+        }
+
+        public void limpiarTxt()
+        {
+            txtCodigo.Text = " ";
+            txtNombre.Text = " ";
+            txtDesc.Text = " ";
+            txtCantidad.Text = " ";
+            txtCosto.Text = " ";
+
+        }
+    }
+}
---
+++ Estructura con Arreglos/Estructura con Arreglos/Productos.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estructura_con_Arreglos
+{
+    class Productos
+    {
+        private int ban = 0;
+        private struct producto{
+            public string codigo;
+            public string nombre;
+            public string descripcion;
+            public string cantidad;
+            public string costo;
+        }
+        producto[] prod = new producto[15];
+
+
+        public void llenar(string codigo, string nombre,
+            string descripcion, string cantidad, string costo)
+        {
+            prod[ban].codigo = codigo;
+            prod[ban].nombre = nombre;
+            prod[ban].descripcion = descripcion;
+            prod[ban].cantidad = cantidad;
+            prod[ban].costo = costo;
+            ban++;
+        }
+
+        public string buscar(string codigo)
+        {
+            bool b = true;
+            string data = "";
+            for(int i = 0; i < 15; i++)
+            {
+                b = prod[0].codigo.Contains(codigo);
+                if (b)
+                {
+                   data += "Codigo:" + prod[i].codigo + "| Nombre: " + prod[i].nombre
+                        + "| Descripcion: " + prod[i].descripcion +"| Cantidad: " + 
+                        prod[i].cantidad + "| Precio: $" + prod[i].costo + "\r\n";
+                }
+            }
+            return data;
+        }
+
+
+        public void insertar(string codigo, string nombre,
+            string descripcion, string cantidad, string costo, int pos)
+        {
+            for (int i = 14; i > pos; i--)
+            {
+                prod[i] = prod[i - 1];
+            }
+            prod[pos].codigo = codigo; prod[pos].nombre = nombre; prod[pos].descripcion = descripcion;
+            prod[pos].cantidad = cantidad; prod[pos].costo = costo;
+        }
+
+        public string Listar()
+        {
+            string data = "";
+            for(int i = 0; i < 15; i++)
+            {
+                data += i+1 + ".- Codigo:" + prod[i].codigo + "| Nombre: " + prod[i].nombre + "| Descripcion: " + prod[i].descripcion +
+                "| Cantidad: " + prod[i].cantidad + "| Precio: $" + prod[i].costo + "\r\n";
+            }
+            return data;
+        }
+    }
+}
